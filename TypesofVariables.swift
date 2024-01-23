db470af479be117bdcf9aef4c00c71ce25c3cd9b
MadhushaPrasad <madushaprasad21@gmail.com>
2024-01-23 12:24:31 +0530

    separate content into files

--- TypesofVariables.swift
+++ TypesofVariables.swift
@@ -1,34 +1,6 @@
 import Foundation
 import UIKit
 
-//variables , constants , static
-
-var str = "Hello World" //variable
-var x = 4 //variable
-var y = 1 //variable
-y = 2 //variable
-
-let z = 3 //constant
-// z = 12 //constant can't be changed/ can't be value reassign
-
-class Person(){ //class
-    
-    static let age = 12 //static -> can't be changed/ can't be value reassign 
-    // and static is a class level variable and it's not a object level variable 
-    // and it's a global variable and it's a constant
-    let lastName = "Prasad" //constant
-    
-}
-
-var prasad = Person() //object
-var silva = Person() //object
-var indudini = Person() //object
-let madhusha = Person.age //constant
-
-var personName = "Madhusha" //variable can be changed/ can be value reassign
-personName = "Prasad"
-
-
 // Types of variables
 
 // 1. String
@@ -96,27 +68,3 @@ var person1 = Person1()
 var person2: Person1 = Person1()
 
 var button : UIButton = UIButton()
-
-// Functions and Parameters
-
-// function without parameters
-func printHello(){
-    print("Hello")
-}
-
-// function with parameters
-func printName(name: String){
-    print(name)
-}
-
-// function with parameters and return type
-func printNameAndReturn(name: String) -> String{ //String is return type
-    return name
-}
-
-// how to call functions
-printHello()
-printName(name: "Madhusha")
-printNameAndReturn(name: "Madhusha")
-
-
